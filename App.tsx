b35408011755cdfec3ce8822ff656d01b44f7d1e
Danskuvv <danielvv@metropolia.fi>
2024-03-05 18:52:04 +0200

    fixed navigation

--- App.tsx
+++ App.tsx
@@ -1,24 +1,23 @@
 import {StatusBar} from 'expo-status-bar';
 import {Platform, SafeAreaView, StyleSheet} from 'react-native';
-import Home from './src/views/Home';
+import Navigator from './src/navigators/Navigator';
 
 const App = () => {
+  console.log('moro');
   return (
     <SafeAreaView style={styles.container}>
-      <Home />
+      <Navigator />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
-
-export default App;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,60 +1,20 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
-import {MediaItemWithOwner} from '../types/DBTypes';
-import MediaRow from '../components/MediaListItem';
-import SingleView from '../components/SingleView';
+import {FlatList} from 'react-native';
+import {NavigationProp, ParamListBase} from '@react-navigation/native';
 import {useMedia} from '../hooks/apiHooks';
+import MediaListItem from '../components/MediaListItem';
 
-const Home = () => {
-  const [selectedItem, setSelectedItem] = useState<MediaItemWithOwner | null>(
-    null,
-  );
+const Home = ({navigation}: {navigation: NavigationProp<ParamListBase>}) => {
   const {mediaArray} = useMedia();
-
-  const renderItem = ({item}) => (
-    <MediaRow
-      key={item.media_id}
-      item={item}
-      setSelectedItem={setSelectedItem}
-    />
-  );
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>My Media</Text>
-      {selectedItem && (
-        <SingleView
-          item={selectedItem}
-          setSelectedItem={(item: MediaItemWithOwner | undefined) =>
-            setSelectedItem(item || null)
-          }
-        />
-      )}
+    <>
       <FlatList
         data={mediaArray}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.media_id.toString()}
+        renderItem={({item}) => (
+          <MediaListItem navigation={navigation} item={item} />
+        )}
       />
-    </View>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 export default Home;
